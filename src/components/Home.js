bdcbfbd5884bb0783bd13dbfd0870dff38888ccc
Fahiye Muhammad <fahiye254@gmail.com>
2025-04-09 16:00:02 +0300

    Learning import and Export in React

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {username} from '../data/user.js'
+import {city} from '../data/user.js'
 
 function Home() {
   return (
-    <div id="home">
+     <div id="home">
       <h1>
         {username} is a Web Developer from {city}
       </h1>
     </div>
   );
 }
+export default Home 
